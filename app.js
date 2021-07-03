8b4ccbbfb0445aee8e41c353fefb456a70af969b
mingmoth <77824905+mingmoth@users.noreply.github.com>
2021-07-03 13:37:30 +0800

    add display category function

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const e = require('express')
 const express = require('express')
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
@@ -15,9 +16,17 @@ app.get('/', (req, res) => {
 })
 
 app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
+
+  const keyword = req.query.keyword[0]
+  const form = req.query
+
   const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+    if (form.keyword.length === 2) {
+      console.log(restaurant.category)
+      return restaurant.category.includes(keyword)
+    } else {
+      return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+    }
   })
   // 顯示無搜尋結果頁面
   if (restaurants.length > 0) {
@@ -30,7 +39,12 @@ app.get('/search', (req, res) => {
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', { restaurant: restaurant })
+  if (restaurant === {}) {
+    res.render('show', { no_results: '<h3>搜尋沒有結果</h3>' })
+  } else {
+    console.log(restaurant)
+    res.render('show', { restaurant: restaurant })
+  }
 })
 
 app.listen(port, () => {
