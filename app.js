533816a983d7925dd67e9a462ce1230ef9fa50fc
mingmoth <mingmoth#gmail.com>
2021-07-06 22:02:05 +0800

    fear: add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
+const e = require('express')
 const express = require('express')
 const exphbs = require('express-handlebars')
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const restaurantData = new Schema({
+  name: {
+    type: String,
+    required: true
+  }
+})
+
 const restaurantList = require('./restaurant.json')
 const app = express()
 const port = 3000
+
+// MongoDB 的 3.1.0 版之前，向資料庫連線時不一定要加上 port，但在 3.1.0 版本後，連線資料庫時一定要加上 port。
+// add { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.connect('mongodb://localhost/restaurant-re', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected')
+})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -15,9 +39,17 @@ app.get('/', (req, res) => {
 })
 
 app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
+
+  const keyword = req.query.keyword[0]
+  const form = req.query
+
   const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+    if (form.keyword.length === 2) {
+      console.log(restaurant.category)
+      return restaurant.category.includes(keyword)
+    } else {
+      return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+    }
   })
   // 顯示無搜尋結果頁面
   if (restaurants.length > 0) {
@@ -30,7 +62,12 @@ app.get('/search', (req, res) => {
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', { restaurant: restaurant })
+  if (restaurant === {}) {
+    res.render('show', { no_results: '<h3>搜尋沒有結果</h3>' })
+  } else {
+    console.log(restaurant)
+    res.render('show', { restaurant: restaurant })
+  }
 })
 
 app.listen(port, () => {
