c5bd9fc1d32d97cf0f2450b474373871c151d4c1
mingmoth <mingmoth#gmail.com>
2021-07-10 17:41:09 +0800

    CRUD inital project

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
-const e = require('express')
 const express = require('express')
 const exphbs = require('express-handlebars')
-
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const restaurantData = new Schema({
-  name: {
-    type: String,
-    required: true
-  }
-})
+const Restaurant = require('./models/restaurantData.js')
 
-const restaurantList = require('./restaurant.json')
 const app = express()
 const port = 3000
 
 // MongoDB 的 3.1.0 版之前，向資料庫連線時不一定要加上 port，但在 3.1.0 版本後，連線資料庫時一定要加上 port。
 // add { useNewUrlParser: true, useUnifiedTopology: true }
-mongoose.connect('mongodb://localhost/restaurant-re', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
 const db = mongoose.connection
 
 db.on('error', () => {
@@ -31,43 +23,99 @@ db.once('open', () => {
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// setting body-parser
+app.use(express.urlencoded({ extended: true }))
+
 app.use(express.static('public'))
 
+
+
 app.get('/', (req, res) => {
-
-  res.render('index', { restaurants: restaurantList.results });
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.error(error))
 })
 
-app.get('/search', (req, res) => {
-
-  const keyword = req.query.keyword[0]
-  const form = req.query
-
-  const restaurants = restaurantList.results.filter(restaurant => {
-    if (form.keyword.length === 2) {
-      console.log(restaurant.category)
-      return restaurant.category.includes(keyword)
-    } else {
-      return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
-    }
-  })
-  // 顯示無搜尋結果頁面
-  if (restaurants.length > 0) {
-    res.render('index', { restaurants: restaurants })
-  } else {
-    res.render('index', { no_results: '<h3>搜尋沒有結果</h3>' })
-  }
-
+app.get('/new', (req, res) => {
+  return res.render('new')
 })
 
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  if (restaurant === {}) {
-    res.render('show', { no_results: '<h3>搜尋沒有結果</h3>' })
-  } else {
-    console.log(restaurant)
-    res.render('show', { restaurant: restaurant })
-  }
+app.post('/restaurants', (req, res) => {
+  console.log('req.body: ', req.body)
+  return Restaurant.create(req.body)
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('detail', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  console.log(id)
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('edit', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+app.post('/restaurants', (req, res) => {
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+
+  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+app.post('/restaurants/:id/edit', (req, res) => {
+
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant.name = name
+      restaurant.name_en = name_en
+      restaurant.category = category
+      restaurant.image = image
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.rating = rating
+      restaurant.google_map = google_map
+      restaurant.description = description
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
+})
+
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
---
+++ models/seeds/dataSeeder.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const restaurantData = require('../restaurantData') // 載入 restaurantData model
+
+const restaurantList = require('../../restaurant.json')
+const raw = restaurantList.results
+
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+  raw.forEach((restaurant) => {
+    restaurantData.create({
+      id: restaurant.id,
+      name: restaurant.name,
+      name_en: restaurant.name_en,
+      category: restaurant.category,
+      image: restaurant.image,
+      location: restaurant.location,
+      phone: restaurant.phone,
+      google_map: restaurant.google_map,
+      rating: restaurant.rating,
+      description: restaurant.description
+    })
+  })
+  console.log('Seed done')
+})
