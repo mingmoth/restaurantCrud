5e9c2bd039deaadb43db5e77bd813093292a9101
mingmoth <mingmoth@gmail.com>
2021-07-19 22:33:23 +0800

    chore: heroku init

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,6 +4,12 @@ mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true,
 
 const db = mongoose.connection
 
+// config/mongoose.js
+// 如果在 Heroku 環境則使用 process.env.MONGODB_URI
+// 否則為本地環境，使用 mongodb://localhost/todo-list
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/todo-list'
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
 db.on('error', () => {
   console.log('mongodb error')
 })
