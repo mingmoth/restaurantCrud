a7164ff38379095b827f2adda9bc0f2693fa0880
mingmoth <mingmoth@gmail.com>
2021-08-22 20:40:26 +0800

    add register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const { rawListeners } = require('../../models/restaurantData')
 const router = express.Router()
 
 router.get('/login', (req, res) => {
   res.render('login')
+})
+
+router.post('/login', (req, res) => {
+
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
 })
 
 module.exports = router
